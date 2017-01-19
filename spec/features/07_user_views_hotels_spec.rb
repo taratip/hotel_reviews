c372baf1bd88545cae188ad42b88b12907b76de3
Tara Jaikaeo <tjaikaeo@gmail.com>
2017-01-19 14:06:04 +0000

    add hotel view

---
+++ spec/features/07_user_views_hotels_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+require 'pry'
+
+feature 'user views hotels', %q(
+  all users can view hotels
+) do
+  let!(:user) { FactoryGirl.create(:user) }
+
+  let!(:hotel1) { FactoryGirl.create(:hotel) }
+  let!(:hotel2) { FactoryGirl.create(:hotel) }
+
+  scenario 'unauthenticated user can view hotels' do
+    visit "/"
+
+    expect(page).to have_content(hotel1.name)
+    expect(page).to have_content(hotel2.name)
+  end
+
+  scenario 'authenticated user can view hotels' do
+    sign_in user
+    visit "/"
+
+    expect(page).to have_content(hotel1.name)
+    expect(page).to have_content(hotel2.name)
+  end
+end
